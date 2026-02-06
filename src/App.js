90db70f056dcc141d289efb73ec135d82420a4e1
Sclpaaaa <chenpascal5@gmail.com>
2026-02-06 15:04:24 +0100

    change mo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> HELLO WORLD.
+          Edit <code>src/App.js</code> HELLO.
         </p>
         <a
           className="App-link"
